d0ca72a90d16cd8793372de0b5a7f4cf5af1ce3f
Paul Blair <ciriwe@phobot.net>
2017-10-22 16:22:28 -0400

    Change remote Unity directory.

--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@ export REMOTE_USER=psfblair
 export REMOTE_HOST=holodevelop
 export REMOTE_PROJECT_DIR='C:\Users\psfblair\Documents\holodevelop\MovingSofaProblem'
 export LOCAL_MOUNT_POINT="/Users/paulblair/Documents/workspaces-Unity/remote"
-export UNITY_FOLDER='C:\Program Files\Unity 5.6.0b9'
+export UNITY_FOLDER='C:\Program Files\Unity'
 export SCENES_TO_BUILD='Assets/Scenes/Room.unity'
 export SOLUTION_FILE_NAME=MovingSofaProblem.sln
