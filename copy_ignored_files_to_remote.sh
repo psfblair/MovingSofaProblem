280a3e42007c5524b91b827597d97d86929550d4
Paul Blair <ciriwe@phobot.net>
2017-10-22 18:44:09 -0400

    Additional script for remote build.

---
+++ copy_ignored_files_to_remote.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+cp Assets/Plugins.meta ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/Plugins.meta
+cp -R Assets/Plugins/ ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/Plugins/
+cp Assets/Fonts.meta ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/Fonts.meta
+cp -R Assets/Fonts/ ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/Fonts/
+cp Assets/SampleRoom.meta ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/SampleRoom.meta
+cp -R Assets/SampleRoom/ ../remote/psfblair/Documents/holodevelop/MovingSofaProblem/Assets/SampleRoom/
